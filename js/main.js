db02103876781f8d2e0044c188a63a40dd88395c
Sokheebjon <sokheebjan@gmail.com>
2020-09-25 00:16:47 +0500

    Portfolio

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                let target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function() {
+                        let $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) {
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1');
+                            $target.focus();
+                        }
+                    });
+                }
+            }
+        });
+
+    var typed = new Typed('.typed', {
+        strings: [
+            'Sokhibjon Ismatov',
+            'a frontend developer',
+            'a programming teacher'
+        ],
+        typeSpeed: 60,
+        backSpeed: 60,
+        loop: true
+    });
+
+})
